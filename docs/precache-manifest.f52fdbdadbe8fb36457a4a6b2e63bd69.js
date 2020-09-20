fea4088717c612fc9e85f835d802c11f61acfeb8
sowa-s <sowashuhei@gmail.com>
2020-09-20 19:50:50 +0900

    add

---
+++ docs/precache-manifest.f52fdbdadbe8fb36457a4a6b2e63bd69.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5304f158faac3fbb2c6cd7381177e9c6",
+    "url": "/project_miskatonic_lp/index.html"
+  },
+  {
+    "revision": "cbaa3385c37c86f5dce6",
+    "url": "/project_miskatonic_lp/static/css/main.e036207f.chunk.css"
+  },
+  {
+    "revision": "851586a021811a67ad0b",
+    "url": "/project_miskatonic_lp/static/js/2.06d56308.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/project_miskatonic_lp/static/js/2.06d56308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbaa3385c37c86f5dce6",
+    "url": "/project_miskatonic_lp/static/js/main.4b1a48d1.chunk.js"
+  },
+  {
+    "revision": "2162cf1f94cf699213cf",
+    "url": "/project_miskatonic_lp/static/js/runtime-main.c6f28b9c.js"
+  },
+  {
+    "revision": "a4823226f32e0e2e4193c46cf9f1bccd",
+    "url": "/project_miskatonic_lp/static/media/CHARACTER.a4823226.png"
+  },
+  {
+    "revision": "9231c47c99a2d7239cff809948da9c56",
+    "url": "/project_miskatonic_lp/static/media/DOWNLOAD.9231c47c.png"
+  },
+  {
+    "revision": "6b9bf35b2d9c825ab0a056d65d1b5f15",
+    "url": "/project_miskatonic_lp/static/media/INFORMATION.6b9bf35b.png"
+  },
+  {
+    "revision": "5b6fa650f9714e57f4df5a53f649538e",
+    "url": "/project_miskatonic_lp/static/media/Project_Miskatonic.5b6fa650.png"
+  },
+  {
+    "revision": "f595f4046e30abe64a04600d28a82191",
+    "url": "/project_miskatonic_lp/static/media/SPEC.f595f404.png"
+  },
+  {
+    "revision": "81acc7d348538c31b2101816263ec74d",
+    "url": "/project_miskatonic_lp/static/media/STORY.81acc7d3.png"
+  },
+  {
+    "revision": "88ec01efddc2f156a0b0b455f3be09c2",
+    "url": "/project_miskatonic_lp/static/media/main.88ec01ef.png"
+  },
+  {
+    "revision": "0724e6deaa91bdf086dc98d0c17b24ea",
+    "url": "/project_miskatonic_lp/static/media/sakata.0724e6de.png"
+  }
+]);
